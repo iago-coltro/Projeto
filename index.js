09ce223f1a95fe04e2f761e35d5c3f202d510ed4
Iago Luciano <iago.luciano@estudante.iftm.edu.br>
2025-12-08 22:50:11 -0300

    Adição do add e modificações

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const app = express();
-const path = require('path');
-const port = 3000;
-const db = require('./db/connection');
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
-app.listen(port, function () {
-  console.log(`Servidor rodando em na porta ${port}`);
+const PORT = 3000;
+
+app.listen(PORT, function() {
+  console.log(`O Express está rodando na porta ${PORT}`);
 });
 
 // body parser
@@ -15,25 +19,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//static folder
+// static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//db connection
-db.authenticate()
+// db connection
+db
+  .authenticate()
   .then(() => {
-    console.log('Conexão com o banco de dados foi estabelecida com sucesso.'); 
+    console.log("Conectou ao banco com sucesso");
   })
   .catch(err => {
-    console.error('Não foi possível conectar ao banco de dados:', err);
+    console.log("Ocorreu um erro ao conectar", err);
   });
 
 // routes
-app.get('/', function (req, res) {
-  res.render('index');
+app.get('/', (req, res) => {
+
+  let search = req.query.job;
+  let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
+
+  
 });
 
-//jobs routes
+// jobs routes
 app.use('/jobs', require('./routes/jobs'));
--- models/Job.js
+++ models/Job.js
@@ -2,26 +2,24 @@ const Sequelize = require('sequelize');
 const db = require('../db/connection');
 
 const Job = db.define('job', {
-    title: {
-        type: Sequelize.STRING,
-    },
-    description: {
-        type: Sequelize.STRING,
-    },
-    salary: {
-        type: Sequelize.STRING,
-    },
-    company: {
-        type: Sequelize.STRING,
-    },
-    email: {
-        type: Sequelize.STRING,
-    },
-    new_job: {
-        type: Sequelize.INTEGER,
-    }
-}, {
-    timestamps: false   // <<----- DESATIVA createdAt e updatedAt
+  title: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  salary: {
+    type: Sequelize.STRING,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  new_job: {
+    type: Sequelize.INTEGER,
+  }
 });
 
-module.exports = Job;
+module.exports = Job
